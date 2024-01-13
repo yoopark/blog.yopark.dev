b96aae034486c3b1b7739cbd20550fc82d3dad0b
Yongjun Park <yopark@student.42seoul.kr>
2024-01-14 04:50:53 +0900

    chore(pkg): install contentlayer & small settings

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
+import { allPosts } from '@contentlayer/generated';
+
 const Home = () => {
-  return <>Hello World</>;
+  return (
+    <div>
+      {allPosts.map((post) => {
+        return (
+          <div key={post._raw.flattenedPath}>
+            <h1>{post.title}</h1>
+            <p>{post.date}</p>
+            <p>{post.description}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Home;
---
+++ contentlayer.config.ts
@@ -0,0 +1,36 @@
+import { defineDocumentType, makeSource } from 'contentlayer/source-files';
+
+export const Post = defineDocumentType(() => ({
+  name: 'Post',
+  filePathPattern: `**/*.md`,
+  fields: {
+    title: {
+      type: 'string',
+      required: true,
+    },
+    date: {
+      type: 'date',
+      required: true,
+    },
+    description: {
+      type: 'string',
+    },
+    thumbnail: {
+      type: 'string',
+    },
+    tags: {
+      type: 'list',
+      of: {
+        type: 'string',
+      },
+    },
+  },
+  computedFields: {
+    url: {
+      type: 'string',
+      resolve: (post) => `/posts/${post._raw.flattenedPath}`,
+    },
+  },
+}));
+
+export default makeSource({ contentDirPath: 'posts', documentTypes: [Post] });
