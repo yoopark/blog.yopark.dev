ed0e33be87a168b512c62b6899154653381c2230
Yongjun Park <yopark@student.42seoul.kr>
2024-01-14 07:33:49 +0900

    design(Header): backdrop-blur

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export const Header = () => {
   return (
-    <header className="fixed h-20 w-full flex items-center top-0 bg-white">
+    <header className="fixed h-20 w-full flex items-center top-0 bg-white/50 backdrop-blur-md">
       <div className="flex w-full items-center justify-between max-w-screen-lg m-auto px-4">
         <h1 className="font-bold text-2xl hover:text-green-700 transition">
           <Link href="/">🌱 Yopark Devlog</Link>
